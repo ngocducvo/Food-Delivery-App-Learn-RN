b0c65519b46f4f888a5f17b00e71df5109127da1
Ngoc Duc Vo <ngducbbdep@gmail.com>
2022-04-09 01:20:26 +0700

    demo stack

--- App.tsx
+++ App.tsx
@@ -1,20 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, View } from 'react-native';
+import { Home, Restaurant, OrderDelivery } from './screens';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createNativeStackNavigator();
 
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{
+					headerShown: false,
+				}}
+				initialRouteName={'Home'}
+			>
+				<Stack.Screen name="Home" component={Home}></Stack.Screen>
+				<Stack.Screen name="Restaurant" component={Restaurant}></Stack.Screen>
+				<Stack.Screen
+					name="OrderDelivery"
+					component={OrderDelivery}
+				></Stack.Screen>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+};
+export default App;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
