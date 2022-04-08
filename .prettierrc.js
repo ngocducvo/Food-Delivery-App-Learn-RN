24076b0b4e1ba9770e2a37d1a554c291d04502da
Ngoc Duc Vo <ngducbbdep@gmail.com>
2022-04-08 12:37:43 +0700

    setup project

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSpacing: true,
+  // jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  // arrowParens: 'avoid',
+  semi: true,
+  endOfLine: "auto",
+  useTabs: true,
+  quoteProps: "preserve",
+};
